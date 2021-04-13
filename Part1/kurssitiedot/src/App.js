a79d8867da74b97e929c9b5086c3cfe9b38aec8b
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-04-12 22:04:46 -0700

    unicafe

--- Part1/kurssitiedot/src/App.js
+++ Part1/kurssitiedot/src/App.js
@@ -1,27 +1,38 @@
 import React from 'react'
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
+	const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 	return (
 		<div>
 			<Header course={course} />
-			<Content part1={part1} ex1={exercises1} part2={part2} ex2={exercises2} part3={part3} ex3={exercises3} />
-			<Total num={exercises1 + exercises2 + exercises3} />
+			<Content course={course} />
+			<Total course={course} />
 		</div>
 	)
 }
 
 const Header = (props) => {
+	console.log(props.course.parts)
 	return (
 		<>
-			<h1>{props.course}</h1>
+			<h1>{props.course.name}</h1>
 		</>
 	)
 }
@@ -29,23 +40,23 @@ const Header = (props) => {
 const Content = (props) => {
 	return (
 		<div>
-			<Part part={props.part1} ex={props.ex1} />
-			<Part part={props.part2} ex={props.ex2} />
-			<Part part={props.part3} ex={props.ex3} />
+			<Part part={props.course.parts[0]} />
+			<Part part={props.course.parts[1]} />
+			<Part part={props.course.parts[2]} />
 		</div>
 	)
 }
 
 const Part = (props) => {
 	return (
-		<p>{props.part} {props.ex}</p>
+		<p>{props.part.name} {props.part.exercises}</p>
 	)
 }
 
 const Total = (props) => {
 	return (
 		<>
-			<p>Number of exercises {props.num}</p>
+			<p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
 		</>
 	)
 }
---
+++ unicafe/src/App.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+
+const Button = ({text, handleButton}) => {
+	return (
+		<button onClick={handleButton}>
+			{text}
+		</button>
+	)
+}
+
+const StatisticLine = ({text, value}) => {
+	return (
+		<tr>
+			<td>{text}</td>
+			<td>{value}</td>
+		</tr>
+	)
+}
+
+
+const Average = ({good, bad, all}) => {
+	let average
+	average = (good + (bad * -1)) / all
+	return (
+		<tr>
+			<td>average</td>
+			<td>{average}</td>
+		</tr>
+	)
+}
+
+const Positive = ({good, all}) => {
+	let pos
+	pos = (good / all) * 100
+	return (
+		<tr>
+			<td>positive</td>
+			<td>{pos} %</td>
+		</tr>
+	)
+}
+
+const Statistics = (props) => {
+	if (props.all === 0)
+		return (
+			<p>No feedback given</p>
+		)
+	return (
+		<table>
+			<tbody>
+			<StatisticLine text='good' value={props.good}/>
+			<StatisticLine text='neutral' value={props.neutral}/>
+			<StatisticLine text='bad' value={props.bad}/>
+			<StatisticLine text='all' value={props.all}/>
+			<Average good={props.good} bad={props.bad} all={props.all} />
+			<Positive good={props.good} all={props.all} />
+			</tbody>
+		</table>
+	)
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+	const [all, setAll] = useState(0)
+
+	const increaseGood = () => {
+		setGood(good + 1)
+		setAll(all + 1)
+	}
+	const increaseNeutral = () => {
+		setNeutral(neutral + 1)
+		setAll(all + 1)
+	}
+	const increaseBad = () => {
+		setBad(bad + 1)	
+		setAll(all + 1)
+	}
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+			<Button text='good' handleButton={increaseGood}/>
+			<Button text='neutral' handleButton={increaseNeutral}/>
+			<Button text='bad' handleButton={increaseBad}/>
+			<h1>statistics</h1>
+			<Statistics good={good} neutral={neutral} bad={bad} all={all}/>
+			
+			
+    </div>
+  )
+}
+
+export default App
